2d51f3c144ce4bf2609379cbe2bd23fdf2bfb305
pizzamann <pizzamann113@gmail.com>
2019-05-31 18:48:57 +0200

    add bash script
    add bash script for easier use of different custom configs

---
+++ nest_them_all.sh
@@ -0,0 +1,11 @@
+# the comments only needed if you use a virtual environment
+#. /path/to/your/venv/bin/activate
+
+nest_conf_folder="custom_configs"
+
+for nest_conf in "$nest_conf_folder"/*
+do
+  echo $nest_conf
+  python analyze_nests.py -c $nest_conf
+done
+#deactivate
