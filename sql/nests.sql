204dbd3cdcf873cb457c49b51ccd345774a89a2a
ccev <42342921+ccev@users.noreply.github.com>
2020-11-09 23:32:18 +0100

    20k varchar for now (idk how to save json)

--- sql/nests.sql
+++ sql/nests.sql
@@ -9,7 +9,7 @@ CREATE TABLE `nests` (
   `pokemon_avg` double DEFAULT 0,
   `pokemon_ratio` double DEFAULT 0,
   `polygon_type` tinyint(1) DEFAULT 0,
-  `polygon_path` varchar(2000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `polygon_path` varchar(20000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `type` tinyint(1) NOT NULL DEFAULT 0,
   `updated` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`nest_id`),
