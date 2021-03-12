8c37ffeedb8ec652d0c24d5290e5eea6f548af26
ccev <42342921+ccev@users.noreply.github.com>
2021-03-13 00:42:33 +0100

    allow str in config, not bool

--- nestwatcher/config.py
+++ nestwatcher/config.py
@@ -10,7 +10,7 @@ class Config:
         self.use_events = config_file.getboolean("Config", "events", fallback=True)
         self.hemisphere = config_file.get("Config", "hemisphere", fallback="all")
         self.less_queries = config_file.getboolean("Config", "less_queries", fallback=False)
-        self.submitted_by = config_file.getboolean("Config", "bot_name", fallback=None)
+        self.submitted_by = config_file.get("Config", "bot_name", fallback=None)
         if not self.submitted_by:
             self.submitted_by = None
         self.pokestop_pokemon = config_file.getboolean("Config", "pokestop_pokemon")
